6823cad47ee7f923480d8b4413396b84d0c0ab49
Jashema Panapa <jcpanapa@gmail.com>
2020-03-28 11:27:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,5 +8,5 @@ def batch_badge_creator(names)
   names.each do |name| 
     badge <<  badge_maker("Hello, my name is")# add a greeting for that name
   end
-  return greetings # return the array of all greetings, at the end
+  return badge # return the array of all greetings, at the end
 end
