3de151a6ab142c0513ea5814096bb06d089aa7f6
Jashema Panapa <jcpanapa@gmail.com>
2020-03-28 11:23:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creater(names)
+def batch_badge_creator(names)
   names.map { |name| name = "Hello, my name is #{name}." }
 end
